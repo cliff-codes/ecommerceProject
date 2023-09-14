dfaf8ba5ccfa89e53c865eb4a0ca32d72600c5a0
Simple Codes <simplecodes2580@gmail.com>
2023-09-13 20:51:39 -0700

    client side auth implemented in the authScreen

--- client/src/uiComponents/AuthScreen.jsx
+++ client/src/uiComponents/AuthScreen.jsx
@@ -1,22 +1,111 @@
-import { Box, Button, FormLabel, Input, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, Input, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import Logo from './Logo'
 import GoogleIcon from '@mui/icons-material/Google';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import validator from 'validator'
 
 const AuthScreen = () => {
-  return (
-    <Box display={'flex'} gap={'8px'} alignItems={'center'} flexDirection={'column'} marginBottom={'16px'}>
-        <Logo/>
+  const [email, setEmail] = useState()
+  const [phone, setPhone] = useState()
+  const [password, setPassword] = useState()
+  const [isValid, setIsValid] = useState(null)
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+    //checking if email is valid
+    setIsValid(validator.isEmail(email))
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value)
+  }
+
+  const LoginSection = () => {
+    return <>
         <Typography mb={'16px'}>Login</Typography>
-        <Input placeholder='email' />
-        <Input placeholder='password' sx={{mb: "16px"}}/>
-        <Button sx={{color: "black", border: "1px solid black", "&:hover": {bgcolor: "black", color: "white"}}}>Sign-In</Button>
+        <Input 
+          value={email}
+          type='text' 
+          placeholder='email' 
+          onChange={handleEmailChange}
+          error = {isValid !== null && !isValid}
+          sx={{minWidth: "230px"}}
+        />
+        <Input 
+          value={password}
+          placeholder='password' 
+          sx={{mb: "16px", minWidth: "230px"}}
+          onChange={handlePasswordChange}
+        />
+        <Button 
+          sx={{color: "black", border: "1px solid black", "&:hover": {bgcolor: "black", color: "white"}}}
+          >Sign-In</Button>
         <Typography fontSize={'14px'} mt={'32px'}>sign-in by</Typography>
         <Box display={'flex'}>
             <Button sx={{color: "black"}}><GoogleIcon/></Button>
             <Button sx={{color: "black"}}><FacebookIcon/></Button>
         </Box>
+        <Button  
+          sx={{
+            color: 'black',
+            fontSize: '10px', 
+            bgcolor: "#d4d4d4"
+            }}
+          onClick={mountSignUp}
+          >sign-up</Button>
+    </>
+  }  
+
+  
+  //state of which screen should be displayed. login/sign up
+  const [signUp, setSignUp] = useState(false)
+  const mountSignUp = () => {
+    setSignUp(true)
+  }
+  const signUpSection = () => {
+    return <>
+        <Typography mb={'16px'}>sign-Up</Typography>
+        <Input 
+          value={email}
+          type='text' 
+          placeholder='email' 
+          onChange={handleEmailChange}
+          error = {isValid !== null && !isValid}
+          sx={{minWidth: "230px"}}
+        />
+        <Input 
+          value={password}
+          placeholder='password' 
+          sx={{ minWidth: "230px"}}
+          onChange={handlePasswordChange}
+        />
+        <Input 
+          value={phone}
+          placeholder='phone'
+          sx={{minWidth: "230px"}}
+        />
+        <Button sx={{color: "black", border: "1px solid black", "&:hover": {bgcolor: "black", color: "white"}}}>sign-up</Button>
+
+        <Button  
+          sx={{
+            color: 'black',
+            fontSize: '10px', 
+            bgcolor: "#d4d4d4"
+            }}
+          onClick={() => setSignUp(false)}
+          >login</Button>
+    </>
+  }
+
+  return (
+    <Box display={'flex'} gap={'8px'} alignItems={'center'} flexDirection={'column'} marginBottom={'16px'}>
+        <Logo/>
+        {signUp ? signUpSection() : LoginSection()}
     </Box>
   )
 }
